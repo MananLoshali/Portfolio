b5517c3eb47e8856896b7902e42b23b93f6304e8
Manan Loshali <loshalimanan@gmail.com>
2024-09-08 23:32:32 +0530

    fixed toggle

--- src/Component/Toggle/Toggle.jsx
+++ src/Component/Toggle/Toggle.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./toggle.css";
 import Sun from "../../images/sun.png";
 import Moon from "../../images/moon.png";
@@ -6,6 +6,20 @@ import { ThemeContext } from "../../context";
 
 const Toggle = () => {
   const theme = useContext(ThemeContext);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 480);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const handleClick = () => {
     theme.dispatch({ type: "TOGGLE" });
   };
@@ -17,7 +31,7 @@ const Toggle = () => {
       <div
         className="t-button"
         onClick={handleClick}
-        style={{ left: theme.state.darkMode ? 4 : 25 }}
+        style={{ left: theme.state.darkMode ? (isSmallScreen ? 4 : 0) : 25 }}
       ></div>
     </div>
   );
